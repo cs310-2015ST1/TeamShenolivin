4e928a8b10a9d30b56f8667dfecd4d2cd324b6f8
Oliver Zhang <bestozhang@gmail.com>
2015-06-08 16:39:19 -0700

    Added basic classes to models

--- RoutePlanner/models.py
+++ RoutePlanner/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+import datetime
+import time
 
-# Create your models here.
+
+class Route:
+    def __init__(self):
+        self.points = []
+
+    def get_points(self):
+        return self.points
+
+
+class RouteManager:
+
+    def __init__(self):
+        self.routes = []
+        self.date = datetime.datetime.now()
+        self.timer = UpdateTimer(self)
+
+    def last_update(self):
+        self.date = datetime.datetime.now()
+        return self.date
+
+    def get_points(self):
+        for route in self.routes:
+            route.getPoints()
+
+
+class UpdateTimer:
+    def __init__(self, manager):
+        self.manager = manager
+        self.time = time.time()
+
+    def spinning(self):
+        while True:
+            if time.time() - self.time > 10000000:
+                self.onTimeOut()
+
+    def onTimeOut(self):
+        return 0
