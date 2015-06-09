1c57101eb9b4f2c3ac09047530c16251fd4df119
Oliver Zhang <bestozhang@gmail.com>
2015-06-08 21:54:11 -0700

    Added more functionality to RouteManager, and basic tests

--- RoutePlanner/models.py
+++ RoutePlanner/models.py
@@ -3,24 +3,57 @@ import datetime
 import time
 
 
+class KMLParser():
+    def __init__(self):
+        self.data = []
+
+    def parse_data(self):
+        self.data.append(Route())
+        return self.data
+
+
 class Route:
     def __init__(self):
         self.points = []
+
+    def add_point(self, point):
+        self.points.append(point)
+
+    def remove_point(self, point):
+        self.points.remove(point)
+
+    def find_point(self, point):
+        return self.points.__contains__(point)
 
     def get_points(self):
         return self.points
 
 
 class RouteManager:
-
     def __init__(self):
         self.routes = []
         self.date = datetime.datetime.now()
+        self.parser = KMLParser()
         self.timer = UpdateTimer(self)
 
-    def last_update(self):
+    def add_route(self, route):
+        self.routes.append(route)
+
+    def remove_route(self, route):
+        self.routes.remove(route)
+
+    def clear_routes(self):
+        self.routes = []
+
+    def find_route(self, route):
+        return self.routes.__contains__(route)
+
+    def update_data(self):
+        self.clear_routes()
+        data = self.parser.parse_data()
+        for route in data:
+            self.add_route(route)
         self.date = datetime.datetime.now()
-        return self.date
 
     def get_points(self):
         for route in self.routes:
@@ -31,11 +64,12 @@ class UpdateTimer:
     def __init__(self, manager):
         self.manager = manager
         self.time = time.time()
+        manager.update_data()
 
     def spinning(self):
         while True:
             if time.time() - self.time > 10000000:
-                self.onTimeOut()
+                self.on_time_out()
 
-    def onTimeOut(self):
-        return 0
+    def on_time_out(self):
+        raise Exception()
