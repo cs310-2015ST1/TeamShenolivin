fbf208ad8447819cccad42fa33f03d615cef6f62
Kenard <kenard.agbanlog@gmail.com>
2015-06-12 17:23:51 -0700

    Added login and logout fuctionality

--- RoutePlanner/views.py
+++ RoutePlanner/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
 from RoutePlanner.forms import UserForm
 from RoutePlanner.models import UserProfile
 
@@ -120,3 +121,12 @@ def user_login(request):
         # No context variables to pass to the template system, hence the
         # blank dictionary object...
         return render(request, 'RoutePlanner/login.html', {})
+    
+    
+
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/RoutePlanner/')
