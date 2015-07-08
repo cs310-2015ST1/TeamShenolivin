215625a29f4f3de833735d60c9f76a23a2b4bc41
Kenard <kenard.agbanlog@gmail.com>
2015-07-08 16:23:59 -0700

    Some cleanup of unused code

--- RoutePlanner/forms.py
+++ RoutePlanner/forms.py
@@ -4,12 +4,7 @@ from RoutePlanner.models import UserProfile
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget = forms.PasswordInput())
-    
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
         
-#class UserProfileForm(forms.ModelForm):
-#    class Meta:
-#        model = UserProfile
-#        fields = ()
