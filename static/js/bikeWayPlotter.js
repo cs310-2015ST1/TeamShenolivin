6d96471b7add4126cd2b074787ae49964c00539f
sharonxy <yang_sharon@live.ca>
2015-07-13 14:39:21 -0700

    removed unnecessary lines -S

--- static/js/bikeWayPlotter.js
+++ static/js/bikeWayPlotter.js
@@ -2,14 +2,11 @@ function plotBikeWay(BikeWay, map) {
     var completeBikeWay = BikeWay;
     var bikeWayCoords = [];
     for (var i = 0; i < completeBikeWay.length; i++) {
-        //for (var j = 0; j < allBikeWays[i].coordinates.length; j++){
+
         var latLong = new google.maps.LatLng(completeBikeWay[i][0], completeBikeWay[i][1]);
-        //console.log(allBikeWays[i][0], allBikeWays[i][1]);
         bikeWayCoords.push(latLong);
 
 
-        //console.log("got here");
-        //};
     }
         var bikeWayLine = new google.maps.Polyline({
             path: bikeWayCoords,
