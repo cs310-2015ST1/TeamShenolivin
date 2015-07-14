1cbd4dc8614c50bef2aedaa6e6a64e455aadd547
Oliver Zhang <bestozhang@gmail.com>
2015-07-13 17:41:39 -0700

    Fixed typo

--- RoutePlanner/admin.py
+++ RoutePlanner/admin.py
@@ -4,4 +4,4 @@ from RoutePlanner.models import UserProfile, BikeWay, Location
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(BikeWay)
-admin.site.register(Location)g
+admin.site.register(Location)
