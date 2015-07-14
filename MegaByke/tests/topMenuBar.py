41623bd8d514416d7ef77f92ac2b238d6fc88bc4
sharonxy <yang_sharon@live.ca>
2015-07-13 17:30:08 -0700

    added tests regarding account creation -S

---
+++ MegaByke/tests/topMenuBar.py
@@ -0,0 +1,16 @@
+__author__ = 'Sharon'
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+
+driver.get("http://104.236.97.57:8888/RoutePlanner/")
+assert "Route" in driver.title
+assert "No results found." not in driver.page_source
+
+#click about
+about = driver.find_element_by_link_text("About")
+about.click()
+
+#driver.close()
