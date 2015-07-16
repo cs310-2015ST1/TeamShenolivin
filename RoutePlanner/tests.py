8b8f513a0df4ef80deb97355e0e43f7c8e94fa33
Oliver Zhang <bestozhang@gmail.com>
2015-07-16 16:26:24 -0700

    moved test file

--- RoutePlanner/tests.py
+++ RoutePlanner/tests.py
@@ -1,5 +1,27 @@
-from django.test import TestCase
-import unittest
-from models import UpdateTimer
+__author__ = 'Oliver'
 
-# Create your tests here.
+import unittest
+from RoutePlanner.models import BikeWay, BikeWayManager
+
+class TestBikeWayManager(unittest.TestCase):
+    
+    def setUp(self):
+        self.manager = BikeWayManager()
+
+    def test_contains_4th_Avenue(self):
+        self.assertTrue(BikeWay.objects.get_or_create(name='4th Ave'))
+        self.assertEqual(BikeWay.objects.filter(name='4th Ave').count(), 2)
+    
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+    
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
